c59f88af9607f5d778b778f263eedac47b4aeb11
Steven Alexander <bogatabeav@gmail.com>
2014-03-27 17:39:09 -0400

    Update config.php

--- Connections/config.php
+++ Connections/config.php
@@ -1,8 +1,8 @@
 <?php
 
-$hostname_currency = "localhost";
-$database_currency = "your data base name";
-$username_currency = "your database username";
-$password_currency = "your database password";
+$host = "localhost";
+$db = "your data base name";
+$user= "your database username";
+$pass = "your database password";
 
 ?>
