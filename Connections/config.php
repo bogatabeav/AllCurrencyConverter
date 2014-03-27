d69216b2f00c9896be1d3cf0fc4e1ed5af9e94d0
Steven Alexander <bogatabeav@gmail.com>
2014-03-27 17:32:00 -0400

    Update config.php

--- Connections/config.php
+++ Connections/config.php
@@ -1,10 +1,8 @@
 <?php
-# FileName="Connection_php_mysql.htm"
-# Type="MYSQL"
-# HTTP="true"
+
 $hostname_currency = "localhost";
 $database_currency = "your data base name";
 $username_currency = "your database username";
 $password_currency = "your database password";
-$currency = mysql_pconnect($hostname_currency, $username_currency, $password_currency) or trigger_error(mysql_error(),E_USER_ERROR); 
+
 ?>
