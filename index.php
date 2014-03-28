d549b786f6680a70a36172db64b9b5c3a5c89d46
Steven Alexander <bogatabeav@gmail.com>
2014-03-27 22:21:46 -0400

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,10 @@ include("header.php");
 <p>
 Content Goes Here!
 <?php
+/* To access database use connect.php to open access
+   and close.php to close access when your PHP script is complete
+   (See below)
+  */
 //include("Connections/connect.php");
 //include("Connections/close.php");
 ?>
