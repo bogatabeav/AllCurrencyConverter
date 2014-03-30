7176869ecb12f638ef4bb5324814d956c3b1ed66
Steven Alexander <bogatabeav@gmail.com>
2014-03-29 23:50:54 -0400

    updates

--- index.php
+++ index.php
@@ -1,10 +1,71 @@
 <?php 
 include("header.php");
+include("Connections/connect.php");
+include("lib/functions.php");
 ?>
-<p>
-Content Goes Here! 
-</p>  
+
+<h1>Currency Converter</h1>
+<form method="post" action="index.php" >
+  <label for="amount">Enter Amount: </label>
+  <input type="text" name="amount" <? if($_POST['amount']>0) {echo "value=\"" . $_POST['amount']."\"";}?> />
+  </br></br>
+  <label for="from">From: </label>
+  <select name="currencyA" id="currencyA">
+    
 <?php
+// see lib/functions.php for getCurrencies() info
+$currencies = getAllCurrencies($con);
+
+foreach($currencies as $value) {
+	if($_POST['currencyA']==$value[1]) {
+		echo "<option value=\"" .$value[1]. "\" selected>".$value[1]."  " .$value[2]. "</option>";
+	} else {
+        echo "<option value=\"" .$value[1]. "\">".$value[1]."  " .$value[2]. "</option>";
+	}	
+}
+?>
+
+  </select>
+  </br></br>
+  <label for="to">To: </label>
+  <select name="currencyB" id="currencyB">
+    
+<?php
+foreach($currencies as $value) {
+	if($_POST['currencyB']==$value[1]) {
+		echo "<option value=\"" .$value[1]. "\" selected>".$value[1]."  "  .$value[2]. "</option>";
+	} else {
+        echo "<option value=\"" .$value[1]. "\">".$value[1]."  "  .$value[2]. "</option>";
+	}	
+}
+?>
+
+  </select>
+  </br></br>
+  <input type="submit" value="Submit" />
+</form></br>
+<?php
+// see lib/functions.php for getRate() info
+$rate1 = getRate($con, $_POST['currencyA']);
+$rate2 = getRate($con, $_POST['currencyB']);
+
+// see lib/functions.php for getCurrency() info
+$name1 = getCurrency($con, $_POST['currencyA']);
+$name2 = getCurrency($con, $_POST['currencyB']);
+ 
+if($_POST['amount']>0 && is_numeric($_POST['amount'])) {     
+	echo $_POST['amount']." " .$name1[2]. "s to " .$name2[2]. "s equals " 
+		.(1/$rate1[2]*$rate2[2]*$_POST['amount']). " " .$name2[2]. "s</br>";
+} else if($_POST['amount']!=NULL && !is_numeric($_POST['amount'])) {
+	echo  "Please enter a numeric amount.";
+} else {
+echo "";
+}
+?>
+</br>
+
+<?php
+include("Connections/close.php");
 include("footer.php");
 ?>
 
--- timeline.php
+++ timeline.php
@@ -1,10 +1,83 @@
 <?php 
 include("header.php");
+include("Connections/connect.php");
+include("lib/functions.php");
 ?>
-<p>
-Content Goes Here! 
-</p>  
+<!--[if lt IE 9]><script language="javascript" type="text/javascript" src="excanvas.js"></script><![endif]--> 
+<script language="javascript" type="text/javascript" src="scripts/jquery.min.js"></script> 
+<script language="javascript" type="text/javascript" src="scripts/jquery.jqplot.min.js"></script>
+<link rel="stylesheet" type="text/css" href="css/jquery.jqplot.css" />
+<h1>Timeline Chart</h1>
+<form method="post" action="timeline.php" >
+  <label for="currency">Select Currency: </label>
+  <select name="currChart" id="currChart">
 <?php
-include("footer.php");
+$currencies = getAllCurrencies($con);
+
+foreach($currencies as $value) {
+	if($_POST['currChart']==$value[1]) {
+		echo "<option value=\"" .$value[1]. "\" selected>".$value[2]. " (" . $value[1]. ")</option>";
+	} else {
+        echo "<option value=\"" .$value[1]. "\">".$value[2]. " (" . $value[1]. ")</option>";
+	}	
+}
 ?>
 
+  </select>
+  </br></br>
+  <input type="radio" name="timePeriod" value="6" <? if($_POST['timePeriod'] == 6) {echo " checked";}?> />
+  6 Hours
+  <input type="radio" name="timePeriod" value="12"<? if($_POST['timePeriod'] == 12) {echo " checked";}?> />
+  12 Hours
+  <input type="radio" name="timePeriod" value="24"<? if($_POST['timePeriod'] == 24) {echo " checked";}?> />
+  24 Hours
+  <input type="radio" name="timePeriod" value="48"<? if($_POST['timePeriod'] == 48) {echo " checked";}?> />
+  48 Hours </br></br>
+  <input type="submit" value="Submit" />
+</form></br>
+<div id="chartdiv" ></div>
+
+<?php
+$history = getNumRates($con, $_POST['currChart'], $_POST['timePeriod']);
+$currency = getCurrency($con, $_POST['currChart']);
+$greatest = 0;
+$lowest = 10;
+?>
+
+<script>
+var history = <? 
+echo "[";
+$count = 0;
+foreach($history as $value) {
+	if($value[2]>$greatest) {$greatest = ($value[2]+$value[2]/64);}
+	if($value[2]<$lowest) {$lowest = ($value[2]-$value[2]/64);}
+	if($count == $_POST['timePeriod']) {
+		echo "['".($count+1)."',".$value[2]."]";
+	} else {
+		echo "['".($count+1)."',".$value[2]."],";
+	}
+	$count++;
+} 
+echo "]";?>;
+
+$.jqplot('chartdiv',  [history],
+{<?php if($_POST['timePeriod']>0) {
+	echo "title: {text: '".$currency[2]." Timeline (".$currency[1].")', show: true},";}
+else {
+	echo "title: {text: '', show: true,},";
+}?>
+
+  axes:{
+	yaxis:{min:<? echo $lowest; ?>, max:<? echo $greatest; ?>},
+  	xaxis:{min:1, max:<? echo $_POST['timePeriod']; ?>},
+
+  },
+  series:[{color:'#5FAB78',lineWidth: 1.5, showMarker:false}]
+});
+</script> 
+</br>
+
+<?php
+include("Connections/close.php");
+include("footer.php");
+?>
