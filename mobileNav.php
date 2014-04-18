7bf761f52aa54d38b8644b1378511761da5e689e
Steven Alexander <bogatabeav@gmail.com>
2014-04-18 08:44:09 -0400

    Delete mobileNav.php

--- mobileNav.php
+++
@@ -1,8 +0,0 @@
-  <div id="header"><h1>Header Div</h1></div>
-  
-<div id="nav">
-  	<ul id="nav">
-    	<li><a href="index.php">Exchange</a></li>
-        <li><a href="mobileTimeline.php">Timeline</a></li>
-	</ul>
-</div>
