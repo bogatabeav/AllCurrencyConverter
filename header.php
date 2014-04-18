6a43fe6ff475315d35fef31f4980078f4c09cfa9
Steven Alexander <bogatabeav@gmail.com>
2014-04-18 08:44:59 -0400

    Update header.php

--- header.php
+++ header.php
@@ -1,25 +1,48 @@
+<?
+include('lib/userAgent.php');
+?>
 <!doctype html>
 <!--[if lt IE 7]> <html class="ie6 oldie"> <![endif]-->
 <!--[if IE 7]>    <html class="ie7 oldie"> <![endif]-->
 <!--[if IE 8]>    <html class="ie8 oldie"> <![endif]-->
 <!--[if gt IE 8]><!-->
-<html class="">
+<html>
 <!--<![endif]-->
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>All Currency Converter</title>
+<link href="css/normalize.css" rel="stylesheet" type="text/css">
 <link href="css/boilerplate.css" rel="stylesheet" type="text/css">
 <link href="css/stylesheet.css" rel="stylesheet" type="text/css">
-
 <!--[if lt IE 9]>
 <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
 </head>
 <body>
 <div class="gridContainer clearfix">
+<div id="header">
+	<h1>Header Div</h1>
+</div>
 <?
-include('lib/userAgent.php');
+if($mobile) {
+?> 
+<div id="nav">
+	<ul id="nav">
+    	<li><a href="index.php">Exchange</a></li>
+        <li><a href="mobileTimeline.php">Timeline</a></li>
+<?
+} else {
 ?>
-<div id="content">
+<div id="nav">
+  	<ul id="nav">
+    	<li><a href="index.php">Exchange</a></li>
+    	<li><a href="timeline.php">Timeline</a></li>
+       	<li><a href="test.php">Logic Test Page</a></li>
+<?
+}
+?>
+	</ul>
+</div>
 
+<div id="content">
