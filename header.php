a6114ec8567fd63c18f02cfa5728483fbf9cabb3
Steven Alexander <bogatabeav@gmail.com>
2014-04-22 17:29:11 -0400

    Repair header.php

--- header.php
+++ header.php
@@ -46,16 +46,3 @@ if($mobile) {
 </div>
 
 <div id="content">
-?>
-<div id="nav">
-  	<ul id="nav">
-    	<li><a href="index.php">Exchange</a></li>
-    	<li><a href="timeline.php">Timeline</a></li>
-       	<li><a href="test.php">Logic Test Page</a></li>
-<?
-}
-?>
-	</ul>
-</div>
-
-<div id="content">
