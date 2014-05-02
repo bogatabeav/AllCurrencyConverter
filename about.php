7d19fa2b969ebde791343a609c62a05265423925
dcac410 <dcac410@gmail.com>
2014-05-02 14:29:14 -0400

    Create about.php
    About page for our currency converter tool

---
+++ about.php
@@ -0,0 +1,72 @@
+<?php 
+include("header.php");
+?>
+
+<fieldset>
+<h2>About Us</h2>
+
+<p>All Currency Converter strives to be one of the most up to date accurate money exchange conversion tools with updates feeding in every hour on the hour. At All Currency Converter our tools are guaranteed to not include ads, cookies, or bloat that many of our competitors use when all you want is a simple conversion between USD to EUR.</p>
+
+<p>All Currency Converter will never ask for your information and will never sell any data that our competitors might. We adhere to our core principals of providing our customers with currency tools, so they can might the right buying decisions whether on vacation or business.</p>
+
+
+</fieldset>
+<br>
+<fieldset>
+<h2>Meet the Team</h2>
+<center>
+
+<table>
+<tr>
+<th>David Caccavo</th>
+<th>Phong Bahn</th>
+<th>Steven Alexander</th>
+</tr>
+<tr>
+<td>
+<ul>
+<li>Program Manager</li>
+<li>Software Engineer</li>
+<li>Software Tester</li>
+</ul>
+</td>
+<td>
+<ul>
+<li>Lead Designer</li>
+<li>Software Engineer</li>
+<li>Web Master</li>
+</ul>
+</td>
+<td>
+<ul>
+<li>Lead Programmer</li>
+<li>Software Designer</li>
+<li>Software Tester</li>
+</ul>
+</td>
+</tr>
+</table>
+
+
+</center>
+</fieldset>
+<br>
+<fieldset>
+<h2>Contact Us</h2>
+
+<center>
+<form action="MAILTO:xxxx@nova.umuc.edu" method="post" enctype="text/plain" class="resource">
+Name: <input type="text" name="name"><br><br>
+Email: <input type="text" name="mail"><br><br>
+Message:<br><textarea name="message" rows="5" cols="40"></textarea><br><br>
+<input type="submit" value="Send">
+<input type="reset" value="Reset">
+</form>
+</center>
+
+</fieldset>
+<br><br>
+
+<?
+include("footer.php");
+?>
